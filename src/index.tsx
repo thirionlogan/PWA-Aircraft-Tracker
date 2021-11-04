e804216d0f909d22c766ecef3898d69cae93dfc3
Logan Thirion <thirionlogan@gmail.com>
2021-11-04 02:23:01 -0500

    firebase

--- src/index.tsx
+++ src/index.tsx
@@ -3,29 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import reportWebVitals from './reportWebVitals';
-
-// Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app';
-import { getAnalytics } from 'firebase/analytics';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: 'AIzaSyDo0Hyv_CCTVzw9qkl2bYQg4X5EFNKCPpQ',
-  authDomain: 'pwa-aircraft-tracker.firebaseapp.com',
-  projectId: 'pwa-aircraft-tracker',
-  storageBucket: 'pwa-aircraft-tracker.appspot.com',
-  messagingSenderId: '1010962118960',
-  appId: '1:1010962118960:web:7cbc5c6f5731f9ddb830f5',
-  measurementId: 'G-M55CY67X9D',
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -42,4 +20,4 @@ serviceWorkerRegistration.register();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
